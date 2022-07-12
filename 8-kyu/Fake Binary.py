ee335ec8352ac013abe98d63ebeb6551e340466e
eTonguzov <etonguzov@gmail.com>
2022-07-12 22:47:25 +0300

    Adds new solutions

---
+++ 8-kyu/Fake Binary.py
@@ -0,0 +1,13 @@
+# DESCRIPTION:
+# Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+#
+# Note: input will never be an empty string
+def fake_bin(x):
+    zero = x[0]
+    num = list(zero)
+
+
+
+
+    return num
+print(fake_bin([56562656532326966666]))
