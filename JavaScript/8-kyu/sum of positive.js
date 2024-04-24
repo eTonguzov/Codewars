9ec7ad5763f74d12c1d524b296cebbbac5909bbb
eTonguzov <etonguzov@gmail.com>
2024-04-24 19:16:20 +0300

    Add new Solutions

--- JavaScript/8-kyu/sum of positive.js
+++ JavaScript/8-kyu/sum of positive.js
@@ -3,7 +3,13 @@
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 Note: if there is nothing to sum, the sum is default to 0.*/
-
+//let arr = [-5,5,-55,10]
 function positiveSum(arr) {
-  
+  let sum = 0;
+  arr.forEach(function(i){
+    if(i>0)
+    {sum = sum + i;}
+  });
+return sum;
 }
+//console.log(positiveSum(arr));
